8178fb2382be0c911341544950320bb85bd55bf6
yangco-le <yanglily@sjtu.edu.cn>
2021-04-21 17:53:21 +0800

    2021-04-21

---
+++ x转十进制/main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char str[105];
+    int x;
+    scanf("%s %d", &str, &x);
+
+    int ans = 0;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] >= '0' && str[i] <= '9') ans = x * ans + str[i] - '0';
+        else if (str[i] >= 'A' && str[i] <= 'F') ans = x * ans + str[i] - 'A' + 10;
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
