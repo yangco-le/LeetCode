c5d918f32568d6d2d81f674f540f0a98db4d809e
yangco-le <yanglily@sjtu.edu.cn>
2021-04-27 15:36:44 +0800

    2021-04-27

---
+++ 4-3 首字母大写/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    while(getline(cin,s)){
+        if(s[0]>='a'&&s[0]<='z') s[0]-=32;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==' '||s[i]=='\t'||s[i]=='\r'||s[i]=='\n'){
+                if(s[i+1]>='a'&&s[i+1]<='z')
+                    s[i+1]-=32;
+            }
+        }
+        cout << s << endl;
+    }
+}
