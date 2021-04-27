a630e4b7d73a243df3a674f7be40c888d2544df1
yangco-le <yanglily@sjtu.edu.cn>
2021-04-27 10:44:19 +0800

    2021-04-27

---
+++ 4.1 特殊乘法/main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    int ans = 0;
+    for(int i = 0; i < str1.size(); i++) {
+        for (int j = 0; j< str2.size(); j++) {
+            ans += (str1[i] - '0') * (str2[j] - '0');
+        }
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
