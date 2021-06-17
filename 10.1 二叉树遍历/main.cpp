8bb4a431e3cdc1c7991bd09838d9aa0d0e18a40f
yangco-le <yanglily@sjtu.edu.cn>
2021-06-17 12:43:11 +0800

    2021-06-17

---
+++ 10.1 二叉树遍历/main.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+struct TreeNode {
+	char data;
+	TreeNode* LeftChild;
+	TreeNode* RightChild;
+	TreeNode(char c) : data(c), LeftChild(NULL), RightChild(NULL) {}
+};
+
+TreeNode* Build(int& pos, string str) {
+	char c = str[pos++];
+	if (c == '#') return NULL;
+	TreeNode* root = new TreeNode(c);
+	root->LeftChild = Build(pos, str);
+	root->RightChild = Build(pos, str);
+	return root;
+}
+
+void InOrder(TreeNode* root) {
+	if (root == NULL) {
+		return;
+	}
+	InOrder(root->LeftChild);
+	cout << root->data<< " ";
+	InOrder(root->RightChild);
+	return;
+}
+
+int main() {
+
+	string str;
+	while (cin >> str) {
+		int pos = 0;
+		TreeNode* root = Build(pos, str);
+		InOrder(root);
+		cout << endl;
+	}
+
+	return 0;
+}
