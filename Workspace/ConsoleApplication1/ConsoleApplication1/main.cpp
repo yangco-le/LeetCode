d33eb7e304a91c7e843658451632b455802f50a5
yangco-le <yanglily@sjtu.edu.cn>
2021-06-26 17:01:31 +0800

    2021-06-26

---
+++ Workspace/ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+struct TreeNode {
+	int data;
+	TreeNode* lc;
+	TreeNode* rc;
+	TreeNode(int d) {
+		data = d;
+		lc = NULL;
+		rc = NULL;
+	}
+};
+
+TreeNode* Insert(TreeNode* root, int x, int father) {
+	if (root == NULL) {
+		cout << father << endl;
+		root = new TreeNode(x);
+		return root;
+	}
+	else if (x < root->data) {
+		root->lc = Insert(root->lc, x, root->data);
+	}
+	else {
+		root->rc = Insert(root->rc, x, root->data);
+	}
+	return root;
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+	
+	int tmp;
+	TreeNode* root = NULL;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		root = Insert(root, tmp, -1);
+	}
+	cout << endl;
+
+	return 0;
+}
