6cb6b94073872e0dcabae1b2c79230d873e33302
yangco-le <yanglily@sjtu.edu.cn>
2021-06-27 17:12:49 +0800

    2021-06-27

---
+++ 7-1 代理服务器/main.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<unordered_map>
+#include<string>
+
+using namespace std;
+
+unordered_map<string, bool> m;
+
+bool AllTrue() {
+	for (auto it = m.begin(); it != m.end(); it++) {
+		if (it->second == false) return false;
+	}
+	return true;
+}
+
+void Reset() {
+	for (auto it = m.begin(); it != m.end(); it++) {
+		it->second = false;
+	}
+}
+
+int main() {
+
+	int n;
+	string str;
+	while (cin >> n) {
+		for (int i = 0; i < n; i++) {
+			cin >> str;
+			m[str] = false;
+		}
+		int cnt = 0;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> str;
+			if (m.find(str) != m.end()) {
+				m[str] = true;
+				if (AllTrue()) {
+					Reset();
+					m[str] = true;
+					if (AllTrue()) {
+						cout << -1 << endl;
+						return 0;
+					}
+					cnt++;
+				}
+			}
+		}
+		cout << cnt << endl;
+	}
+
+	return 0;
+}
