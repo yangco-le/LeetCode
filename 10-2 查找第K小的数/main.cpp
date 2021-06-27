b9f77a45a26c8be9cfe29141a6981ddd61f11c1e
yangco-le <yanglily@sjtu.edu.cn>
2021-06-27 10:02:50 +0800

    2021-06-27

---
+++ 10-2 查找第K小的数/main.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<queue>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	while (cin >> n) {
+		priority_queue < int, vector<int>, greater<int> > q;
+		for (int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+			q.push(x);
+		}
+
+		int k, tmp;
+		cin >> k;
+		for (int i = 0; i < k; i++) {
+			tmp = q.top(); q.pop();
+			while (q.top() == tmp) {
+				q.pop();
+			}
+		}
+		cout << tmp << endl;
+	}
+
+	return 0;
+}
