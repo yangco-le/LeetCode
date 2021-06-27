a442e9f6531c2e3849902c441b1f2b1741db800a
yangco-le <yanglily@sjtu.edu.cn>
2021-06-27 12:01:51 +0800

    2021-06-27

---
+++ 10.9 子串计算/main.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<map>
+
+using namespace std;
+
+int main() {
+
+	string str;
+	while (cin >> str) {
+		map<string, int> m;
+		for (int i = 0; i < str.size(); i++) {
+			for (int j = i; j < str.size(); j++) {
+				string key = str.substr(i, j - i + 1);
+				m[key] += 1;
+			}
+		}
+
+		for (auto it = m.begin(); it != m.end(); it++) {
+			if (it->second <= 1) continue;
+			cout << it->first << " " << it->second << endl;
+		}
+	}
+
+	return 0;
+}
