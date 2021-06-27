910b7e939b815661dc760a8a7a90cc699fad288e
yangco-le <yanglily@sjtu.edu.cn>
2021-06-27 09:15:15 +0800

    2021-06-27

---
+++ 10.6 哈弗曼树/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<queue>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	while (cin >> n) {
+		priority_queue<int, vector<int>, greater<int>> q;
+		for (int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+			q.push(x);
+		}
+
+		int res = 0;
+		while (q.size() > 1) {
+			int a = q.top();
+			q.pop();
+			int b = q.top();
+			q.pop();
+			res += a + b;
+			q.push(a + b);
+		}
+		cout << res << endl;
+	}
+
+	return 0;
+}
