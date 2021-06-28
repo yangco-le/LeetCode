131f7735204230a9c2178f80400e3560d1ff9f23
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 14:42:23 +0800

    2021-06-28

---
+++ 全排列/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string s;
+    while (cin >> s) {
+        sort(s.begin(), s.end());
+        cout << s << endl;
+        for (; next_permutation(s.begin(), s.end());) {
+            cout << s << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
