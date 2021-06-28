02c0b9e88657f039031b9e2b82451c746eac4a7d
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 14:56:51 +0800

    2021-06-28

---
+++ 二叉树/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int F(int n, int m) {
+    if (m > n) {
+        return 0;
+    }
+    return F(n, 2 * m) + F(n, 2 * m + 1) + 1;
+}
+
+int main() {
+
+    int n, m;
+    while (cin >> m >> n) {
+        if (n == 0 && m == 0) break;
+        cout << F(n, m) << endl;
+    }
+
+    return 0;
+}
