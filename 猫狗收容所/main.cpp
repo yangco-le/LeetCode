ea099b463d41f8ee935f013fee444956edd59f9a
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 21:02:49 +0800

    2021-06-28

---
+++ 猫狗收容所/main.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<queue>
+
+using namespace std;
+
+struct animal {
+	int id;
+	int time;
+	animal(int a, int b) {
+		id = a;
+		time = b;
+	}
+};
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	queue<animal> dog;
+	queue<animal> cat;
+
+	for (int i = 0; i < n; i++) {
+		int m, t;
+		cin >> m >> t;
+		if (m == 1) {
+			if (t > 0) {
+				dog.push(animal(t, i));
+			}
+			else if (t < 0) {
+				cat.push(animal(t, i));
+			}
+		}
+		else if (m == 2) {
+			if (t == 1 && !dog.empty()) {
+				cout << dog.front().id << " ";
+				dog.pop();
+			}
+			else if (t == -1 && !cat.empty()) {
+				cout << cat.front().id << " ";
+				cat.pop();
+			}
+			else if (t == 0) {
+				if (dog.empty() && !cat.empty()) {
+					cout << cat.front().id << " ";
+					cat.pop();
+				}
+				else if (cat.empty() && !dog.empty()) {
+					cout << dog.front().id << " ";
+					dog.pop();
+				}
+				else if (dog.front().time < cat.front().time && !dog.empty() && !cat.empty()) {
+					cout << dog.front().id << " ";
+					dog.pop();
+				}
+				else if (!dog.empty() && !cat.empty()) {
+					cout << cat.front().id << " ";
+					cat.pop();
+				}
+			}
+		}
+	}
+
+	return 0;
+}
