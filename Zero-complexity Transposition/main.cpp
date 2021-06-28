431b76296ccb8bd1e1bc833f9d24f42964f4b385
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 21:34:09 +0800

    2021-06-28

---
+++ Zero-complexity Transposition/main.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<stack>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    stack<int> s;
+
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        s.push(tmp);
+    }
+
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+
+    return 0;
+}
