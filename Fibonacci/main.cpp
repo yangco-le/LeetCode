333b3ef204529ce40044ee40ce104a8f44bcaa49
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 14:45:53 +0800

    2021-06-28

---
+++ Fibonacci/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+
+int main() {
+
+    int dp[35];
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= 30; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    int n;
+    cin >> n;
+    cout << dp[n] << endl;
+
+    return 0;
+}
