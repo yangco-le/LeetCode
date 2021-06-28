a33262b555c292069da28f997d1a7504ef88694f
yangco-le <yanglily@sjtu.edu.cn>
2021-06-28 13:23:58 +0800

    2021-06-28

---
+++ 8.2 汉诺塔3/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+
+#define ll long long 
+using namespace std;
+
+ll Function(int n) {
+	if (n == 1) return 2;
+	return 3 * Function(n - 1) + 2;
+}
+
+
+int main() {
+
+	int n;
+	while (cin >> n) {
+		cout << Function(n) << endl;
+	}
+
+	return 0;
+}
