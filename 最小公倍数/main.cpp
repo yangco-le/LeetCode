fdc16219f028b855542515d2cd193fd783bad41d
yangco-le <yanglily@sjtu.edu.cn>
2021-06-29 21:28:03 +0800

    2021-06-29

---
+++ 最小公倍数/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+int GCD(int a, int b) {
+    return (b == 0) ? a : GCD(b, a % b);
+}
+
+int main() {
+
+    int a, b;
+    while (cin >> a >> b) {
+        cout << a*b/GCD(a, b) << endl;
+    }
+
+    return 0;
+}
