b9d4d233613998138201bd0300cf043f5ac8ad5c
yangco-le <yanglily@sjtu.edu.cn>
2021-06-29 21:14:56 +0800

    2021-06-29

---
+++ 进制转换4/main.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+    int M, N;
+    while (cin >> M >> N) {
+        string str;
+        cin >> str;
+        long long res = 0;
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] <= '9' && str[i] >= '0')
+                res = res * M + str[i] - '0';
+            else {
+                res = res * M + str[i] - 'A' + 10;
+            }
+        }
+
+        vector<char> ans;
+        while (res != 0) {
+            if (res % N < 10) {
+                ans.push_back(res % N + '0');
+            }
+            else {
+                ans.push_back(res % N - 10 + 'a');
+            }
+            res /= N;
+        }
+
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            cout << ans[i];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
