d8886e8d597521f80a18ef65764df0d0a187b4d1
yangco-le <yanglily@sjtu.edu.cn>
2021-06-29 21:36:35 +0800

    2021-06-29

---
+++ 素数判定/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    while (cin >> n) {
+        if (n == 1) {
+            cout << "no" << endl;
+            continue;
+        }
+        bool flag = true;
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
+}
