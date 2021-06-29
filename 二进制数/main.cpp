a78c6bc54e1d537b8516da1a8f6f4690573d84a1
yangco-le <yanglily@sjtu.edu.cn>
2021-06-29 14:22:40 +0800

    2021-06-29

---
+++ 二进制数/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    int res[105];
+    unsigned int n;
+    while (cin >> n) {
+        int index = 0;
+        while (n != 0) {
+            res[index++] = n % 2;
+            n /= 2;
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            cout << res[i];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
