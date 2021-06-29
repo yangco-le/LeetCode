9280e0921be8a6018ea8eec0dc7348884adc0cb0
yangco-le <yanglily@sjtu.edu.cn>
2021-06-29 14:37:19 +0800

    2021-06-29

--- 二进制数/main.cpp
+++ 二进制数/main.cpp
@@ -1,25 +1,23 @@
 ﻿#include<iostream>
 #include<cstdio>
 #include<string>
+#include<cstring>
+#include<vector>
 
 using namespace std;
 
 int main() {
-
-    int res[105];
-    unsigned int n;
-    while (cin >> n) {
-        int index = 0;
-        while (n != 0) {
-            res[index++] = n % 2;
-            n /= 2;
-        }
-
-        for (int i = index - 1; i >= 0; i--) {
-            cout << res[i];
-        }
-        cout << endl;
-    }
-
-    return 0;
+	unsigned int n;
+	while (cin >> n) {
+		vector<int> res;
+		while (n != 0) {
+			res.push_back(n % 2);
+			n /= 2;
+		}
+		for (int i = res.size() - 1; i >= 0; i--) {
+			cout << res[i];
+		}
+		cout << endl;
+	}
+	return 0;
 }
