ee67a4de0c9903a29cd675e58e7dcf957c42e80a
yangco-le <yanglily@sjtu.edu.cn>
2021-07-04 12:21:11 +0800

    2021-07-04

---
+++ Two Sum/main.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(target - nums[i]) == mp.end()) {
+                mp[nums[i]] = i;
+            }
+            else {
+                res.push_back(mp[target - nums[i]]);
+                res.push_back(i);
+                break;
+            }
+        }
+        return res;
+    }
+};
