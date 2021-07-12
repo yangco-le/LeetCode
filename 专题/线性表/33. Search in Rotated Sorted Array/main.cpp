11628397d9dbf967da4c590b9abd893f36b288f2
yangco-le <yanglily@sjtu.edu.cn>
2021-07-12 20:25:16 +0800

    2021-07-12

---
+++ 专题/线性表/33. Search in Rotated Sorted Array/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int first = 0; int last = nums.size(), mid;
+        while (first != last) {
+            mid = first + (last - first) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[first] <= nums[mid]) {
+                if (nums[first] <= target && target < nums[mid]) {
+                    last = mid;
+                }
+                else {
+                    first = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] <= target && target <= nums[last-1]) {
+                    first = mid + 1;
+                }
+                else {
+                    last = mid;
+                }
+            }
+        }
+        return -1;
+    }
+};
